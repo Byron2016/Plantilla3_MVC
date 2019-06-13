5f5f5740e458884c2b3ac1246dfbcb9cd4a2df26
Byron <bgva2005@yahoo.com>
2019-06-13 00:05:47 -0500

    fin video 02

---
+++ public/controllers/indexController.php
@@ -0,0 +1,13 @@
+<?php
+
+//v2
+
+class indexController extends Controller   //v2
+{
+    
+	public function index()                //v2
+	{
+        echo "Hola desde index Controler"; //v2
+
+	}
+}
